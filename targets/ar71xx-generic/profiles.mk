833ee39bc319c377bb1de5ae07f9f4d3ef772e00
Franz Pletz <fpletz@fnordicwalking.de>
2015-09-09 22:32:52 +0200

    Add support for Ubiquity Air Gateway

--- targets/ar71xx-generic/profiles.mk
+++ targets/ar71xx-generic/profiles.mk
@@ -135,6 +135,8 @@ endif
 
 ## Ubiquiti (everything)
 $(eval $(call GluonProfile,UBNT))
+$(eval $(call GluonModel,UBNT,ubnt-air-gateway,ubiquiti-air-gateway))
+
 $(eval $(call GluonModel,UBNT,ubnt-bullet-m,ubiquiti-bullet-m))
 $(eval $(call GluonModel,UBNT,ubnt-bullet-m,ubiquiti-loco-m))
 $(eval $(call GluonModel,UBNT,ubnt-bullet-m,ubiquiti-picostation-m))
